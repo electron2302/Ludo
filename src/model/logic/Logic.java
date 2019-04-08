cd8775bfe10bffecbf3bccd6230d25b0f955c68c
AndHager <48909788+AndHager@users.noreply.github.com>
2019-04-08 22:05:56 +0200

    Implementation and testing the dice.

--- src/model/logic/Logic.java
+++ src/model/logic/Logic.java
@@ -1,5 +1,14 @@
 package model.logic;
-
+/**
+ * 
+ * @author A. Hager, ahager@hm.edu
+ *
+ */
 public interface Logic {
-
+	boolean move(int tockenID, int diceValue);
+	boolean init();
+	public static int throwDice() {
+		return (int) ((Math.random()*5897)%6)+1;
+	}
+	
 }
